078f883bc09a711e1aa510505fc5066f254f66e9
Erik Ytterman <erik.ytterman@phaseshift.se>
2016-07-18 15:36:10 +0200

    Basic filtering using regex and 'match'

--- WordStats.scala
+++ WordStats.scala
@@ -1,16 +1,25 @@
 import scala.io.Source
+import scala.util.matching
+
 import java.io.{FileReader, FileNotFoundException, IOException}
 
 object WordStats {
-   def main(args: Array[String]) {
-      val filename = "./ord.utf8"
+  def main(args: Array[String]) {
+    val filename = "./ord.utf8"
+    val regex = """ar.b.*""".r
 
-      try {
-         val words = Source.fromFile(filename).getLines.toArray
- 	 words.foreach(println)
-      } catch {
-         case ex: FileNotFoundException => println("Couldn't find that file.")
-         case ex: IOException => println("Had an IOException trying to read that file" + ex)
+    try {
+      val words = Source.fromFile(filename).getLines.toArray
+
+      for(word <- words) {
+        word match {
+          case regex(_*) => println(word)
+          case _ =>
+        }
       }
-   }  
+    } catch {
+      case ex: FileNotFoundException => println("Couldn't find that file.")
+      case ex: IOException => println("Had an IOException trying to read that file" + ex)
+    }
+  }
 }
