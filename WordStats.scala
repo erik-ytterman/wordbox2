15aad4d478efeecf7cd4bfceca264affec8ca226
Erik Ytterman <erik.ytterman@phaseshift.se>
2016-07-18 14:43:44 +0200

    Read all words into array...

--- WordStats.scala
+++ WordStats.scala
@@ -1,10 +1,16 @@
+import scala.io.Source
+import java.io.{FileReader, FileNotFoundException, IOException}
+
 object WordStats {
-   def integers = List(0,1,1,2,3,4,5,6,7,8,9,10)
-
    def main(args: Array[String]) {
-      val I = 11 :: this.integers
+      val filename = "./ord.utf8"
 
-      def evens = I.filter((number: Int) => ((number % 2) == 0)) 
-      evens.foreach(println)
+      try {
+         val words = Source.fromFile(filename).getLines.toArray
+ 	 words.foreach(println)
+      } catch {
+         case ex: FileNotFoundException => println("Couldn't find that file.")
+         case ex: IOException => println("Had an IOException trying to read that file" + ex)
+      }
    }  
 }
