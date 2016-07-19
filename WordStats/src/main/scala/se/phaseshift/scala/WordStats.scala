e796aa5f907339cc65ee7836a93dcd8fae90c527
Erik Ytterman <erik.ytterman@phaseshift.se>
2016-07-19 11:14:18 +0200

    Add complete maven project framework...

--- WordStats/src/main/scala/se/phaseshift/scala/WordStats.scala
+++ WordStats/src/main/scala/se/phaseshift/scala/WordStats.scala
@@ -4,19 +4,21 @@ import scala.util.matching
 import java.io.{FileReader, FileNotFoundException, IOException}
 
 object WordStats {
+  val regex = """ar.b.*""".r
+
+  def wordFilter(word: String): Boolean = word match {
+    case regex(_*) => true
+    case "övliga" => true
+    case _ => false
+  }
+
   def main(args: Array[String]) {
     val filename = "./ord.utf8"
-    val regex = """ar.b.*""".r
 
     try {
-      val words = Source.fromFile(filename).getLines.toArray
-
-      for(word <- words) {
-        word match {
-          case regex(_*) => println(word)
-          case _ =>
-        }
-      }
+      val words = Source.fromFile(filename).getLines.toList
+      val filtered = words.filter(wordFilter)
+      filtered.foreach(println)
     } catch {
       case ex: FileNotFoundException => println("Couldn't find that file.")
       case ex: IOException => println("Had an IOException trying to read that file" + ex)
