d9ae1b10666bddd0da2b5d895dfb71787c6f0574
Erik Ytterman <erik.ytterman@phaseshift.se>
2016-08-26 13:26:06 +0200

    Moved REL_1_0 script code into compilable object

--- src/main/scala/se/phaseshift/scala/wordbox2.scala
+++ src/main/scala/se/phaseshift/scala/wordbox2.scala
@@ -7,86 +7,56 @@ import scala.util.Random
 import java.io.{FileReader, FileNotFoundException, IOException}
 
 object WordStats {
-  // Read words from file and filter according to game size
+  // Read words from resource file
   val instream = getClass.getResourceAsStream("/wordlists/Swedish/ord.utf8")
   val words = Source.fromInputStream(instream).getLines.toList
-  val gamesize = 5
-  val valid = words.filter(_.length == gamesize)
+  val gamesize = 4
 
-  // Create random generator
+  // Filter out words of desired length (4)
   val random = new Random(System.currentTimeMillis)
+  val valid = words.filter(_.length == gamesize).toList
 
   // Create list containing the playfield state, one string per pow in the playfield
-  val gamestate = ListBuffer[String]()
+  var gamestate = List[String]()
 
-  // Create helper functions to use gamestate
-  val minlength = () => gamestate.isEmpty match { case false => gamestate.map(_.length).min case true => 0 }
-  val rows = () => for((row, index) <- gamestate.zipWithIndex.toList) yield { (index + 1, row) }
-  val columns = () => for(position <- (1 to minlength()).toList ) yield { (position, gamestate.map(_(position - 1)).mkString) }
+  // Anonymouns accessor functions to expose the result
+  var rows = () => { gamestate }: List[String]
+  var columns = () => { gamestate.transpose.map( _.foldLeft("")(_ + _) ) }: List[String]
 
-  // Define anonymous function that extracts filter strings for each column
-  // The result will be a list containing the starting characters for each column
-  def columnfilters(word: Option[String]) = {
-    (word, gamestate.isEmpty) match {
-      case (None, true) => for(a <- (1 to gamesize).toList ) yield { "" }
-      case (Some(word), true) => for((chr, index) <- word.zipWithIndex.toList ) yield { chr.toString }
-      case (None, false) => for((index, column) <- columns().toList ) yield { column }
-      case (Some(word), false) => for((index, column) <- columns().toList ) yield { column + word(index - 1) }
-    }
-  }
+  // Recursive solution finding function
+  def gamesolver(state: List[String] = List[String]()): Boolean = {
 
-  def columnmatcher(filters: List[String]): List[Boolean] = {
-    for(filter <- filters) yield {
-      valid.find(_.startsWith(filter)) match {
-        case None => false
-        case Some(word) => true
+    if(state.length < gamesize) {
+
+      for(word <- random.shuffle(valid)) {
+        val newstate = state ::: List(word)
+
+        var columnfilters = newstate.transpose.map( _.foldLeft("")(_ + _) )
+        var columnwords = columnfilters.map( columnfilter => valid.filter( _.startsWith(columnfilter) ) )
+        var isvalidcolumn = columnwords.map(! _.isEmpty )
+        var isvalidword = isvalidcolumn.reduce(_ && _)
+
+        if( isvalidword ) { if( gamesolver(newstate) )  { return true } }
       }
+
+      return false
+
+    } else {
+      gamestate = state
+
+      return true
     }
   }
 
-  def wordmatcher(word: String): Boolean = {
-    return columnmatcher(columnfilters(Some(word))).foldLeft(true)(_ && _)
-  }
-
-  def gamereset() = {
-    gamestate.clear()
-  }
-
-  def gamesolver(depth: Int): Boolean = {
-    val possible = valid.filter(wordmatcher(_))
-
-    print(depth)
-
-    if(gamestate.length == gamesize) {
-      println("-------------------------------")
-      println(rows())
-        println(columns())
-      println("-------------------------------")
-      return(true)
-    }
-    else if(possible.isEmpty) {
-      return(false)
-    }
-    else {
-      for(word <- possible) {
-
-        gamestate += possible(random.nextInt(possible.length))
-
-        if(! gamesolver(depth + 1)) {
-          gamestate.remove(gamestate.length - 1)
-        }
-        else {
-          return(true)
-        }
-      }
-    }
-
-    return false
-  }
-
+  // Program entry point
   def main(args: Array[String]) {
     try {
-      gamesolver(0)
+      if( gamesolver() ) {
+        rows().foreach( println(_) )
+      }
+      else {
+        println("Search failed!")
+      }
     } catch {
       case ex: IOException => println("Had an IOException trying to read that file" + ex)
     }
